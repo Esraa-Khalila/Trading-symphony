3212d486553cc456a698897965348cd550055a56
Esraa-khalila <esraa.khalilah@gmail.com>
2022-11-20 17:03:29 +0300

    edit

--- public/js/all-products.js
+++ public/js/all-products.js
@@ -51,66 +51,66 @@ function choose_btn() {
 }
 // change page number every 12 items
 
-const productItems = document.querySelector(".sub_product").children;
-const prev = document.querySelector(".prev");
-const next = document.querySelector(".next");
-const page = document.querySelector(".page-num");
-const maxItem = 12;
-let index = 1;
-let item = 28;  //all product for this category from database
-let count = 0;
-const pagination = Math.ceil(productItems.length / maxItem);
-function Numpage() {
-  count = Math.ceil(item / maxItem);
+// const productItems = document.querySelector(".sub_product").children;
+// const prev = document.querySelector(".prev");
+// const next = document.querySelector(".next");
+// const page = document.querySelector(".page-num");
+// const maxItem = 12;
+// let index = 1;
+// let item = 28;  //all product for this category from database
+// let count = 0;
+// const pagination = Math.ceil(productItems.length / maxItem);
+// function Numpage() {
+//   count = Math.ceil(item / maxItem);
 
-  let pageNumbers = "";
-  for (let i = 1; i <= count; i++) {
-    pageNumbers = pageNumbers + `<span>${i}</span>`;
-  }
-}
+//   let pageNumbers = "";
+//   for (let i = 1; i <= count; i++) {
+//     pageNumbers = pageNumbers + `<span>${i}</span>`;
+//   }
+// }
 
-prev.addEventListener("click", function () {
-  index--;
+// prev.addEventListener("click", function () {
+//   index--;
 
-  check();
-  showItems();
-});
-next.addEventListener("click", function () {
-  index++;
-  check();
-  showItems();
-});
+//   check();
+//   showItems();
+// });
+// next.addEventListener("click", function () {
+//   index++;
+//   check();
+//   showItems();
+// });
 
-function check() {
-  if (index == pagination) {
-    next.classList.add("disabled");
-  } else {
-    next.classList.remove("disabled");
-  }
+// function check() {
+//   if (index == pagination) {
+//     next.classList.add("disabled");
+//   } else {
+//     next.classList.remove("disabled");
+//   }
 
-  if (index == 1) {
-    prev.classList.add("disabled");
-  } else {
-    prev.classList.remove("disabled");
-  }
-}
+//   if (index == 1) {
+//     prev.classList.add("disabled");
+//   } else {
+//     prev.classList.remove("disabled");
+//   }
+// }
 
-function showItems() {
-  for (let i = 0; i < productItems.length; i++) {
-    productItems[i].classList.remove("show");
-    productItems[i].classList.add("hide");
+// function showItems() {
+//   for (let i = 0; i < productItems.length; i++) {
+//     productItems[i].classList.remove("show");
+//     productItems[i].classList.add("hide");
 
-    if (i >= index * maxItem - maxItem && i < index * maxItem) {
-      productItems[i].classList.remove("hide");
-      productItems[i].classList.add("show");
-    }
-    page.innerHTML = index;
-  }
-}
+//     if (i >= index * maxItem - maxItem && i < index * maxItem) {
+//       productItems[i].classList.remove("hide");
+//       productItems[i].classList.add("show");
+//     }
+//     page.innerHTML = index;
+//   }
+// }
 
 window.onload = function () {
-  showItems();
-  check();
-  Numpage();
+  // showItems();
+  // check();
+  // Numpage();
   choose_btn();
 };
